ba49e0982b83874ec71a52af6c689702ab40d56a
ohserin <dhtpfls321@gmail.com>
2024-11-18 17:26:32 +0900

    Update Monster class attack method and printData

--- src/units/Monster.java
+++ src/units/Monster.java
@@ -1,6 +1,7 @@
 package units;
 
 import java.io.BufferedWriter;
+import java.io.IOException;
 import java.io.OutputStreamWriter;
 
 public abstract class Monster {
@@ -9,7 +10,7 @@ public abstract class Monster {
 	protected int power;
 	protected String name;
 	protected String state = "노말";
-	
+
 	protected BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(System.out));
 
 	Monster(String name, int hp, int att) {
@@ -31,13 +32,33 @@ public abstract class Monster {
 		this.maxHp = hp;
 		this.power = att;
 	}
-	
+
 	public int getCurHp() {
 		return this.curHp;
 	}
 
 	public void attack(Unit target) {
 		target.hp -= (power - target.defense);
-		
+		try {
+			writer.write("------------------------------------------------------");
+			writer.write(String.format("%s가 %s에게 %d만큼의 데미지를 입혔습니다", name, target.name, (power - target.defense)));
+			if (target.hp <= 0) {
+				writer.write(String.format("%s를 처치했습니다.", target.name));
+				target.hp = 0;
+			}
+			writer.flush();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	public void printData() {
+		try {
+			writer.write(String.format("name:[%s] hp:[%d/%d]power:[%d]", name, curHp, maxHp, power));
+			writer.flush();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 }
